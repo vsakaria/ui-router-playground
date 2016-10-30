b951e71773770ed604ae48b37660ee3fc8576a56
Vishal Sakaria <vsakaria@gmail.com>
2016-10-30 14:06:50 +0000

    First commit

--- app/app.js
+++ app/app.js
@@ -2,13 +2,61 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+  'myApp.version',
+  'ui.router'
+])
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+.config([ '$stateProvider', function( $stateProvider) {
+
+  	var helloState = {
+		    name: 'hello',
+		    url: '/hello',
+		    template: '<h3>hello world!</h3>'
+		};
+
+	var aboutState = {
+			name: 'about',
+			url: '/about',
+			component: 'about'
+		};
+
+	var people = {
+			name: 'people',
+		  	url: '/people',
+			template: '<h3>People</h3>'		  
+		};
+
+	$stateProvider.state(helloState);
+	$stateProvider.state(aboutState);
+	$stateProvider.state(people);
+
+}])
+
+.component('about', {
+	template:  '<h3>Its the UI-Router<br>Hello Solar System app!</h3>'
+})
+
+// .component('people', {
+// 	template:  '<h3>People of the Solar System!</h3>',
+
+//     controller: function() {
+// 	    this.greeting = 'People';
+	  
+// 	    this.toggleGreeting = function() {
+// 	    	this.greeting = (this.greeting == 'hello') ? 'whats up' : 'hello';
+// 	    }
+// 	}
+// })
+
+.service('PeopleSerivce', function ($http, $q) {
+	var BASE_URL = "http://localhost:3000/db";
+
+	this.getAllPeople = function () {
+		$http.get(BASE_URL)
+			.then(this.successCallback);
+	}
+
+	this.successCallback = function (response) {
+		return response;
+	}
+})
